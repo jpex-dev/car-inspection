6954dd654c063893723e5b4ad9937ddab8925462
João Peixoto <joao@jpex.dev>
2023-05-15 12:31:10 +0100

    ja esta

--- newtry.php
+++ newtry.php
@@ -3,13 +3,14 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <style>
     .input-group {
+        margin-left: 20%;
+        margin-right: 15%;
         display: flex;
-        /* torna o container um flexbox */
-        justify-content: flex-end;
-        /* alinha os inputs à direita */
-        max-width: 50%;
-        /* define a largura máxima do contêiner */
-        margin-left: 15%;
+
+    }
+
+    .timer {
+        width: 80px;
     }
 
     .input-group input {
@@ -19,52 +20,37 @@
         /* adiciona um espaçamento entre os inputs */
     }
 
-    .dropbtn {
-        background-color: white;
-        color: black;
-        padding: 10px;
-        width: 400px;
-        height: 40px;
-        font-size: 14px;
-        border: solid;
-        cursor: pointer;
-        border-width: 0.5px;
-
-    }
-    .flatpickr:focus{  
-        border-top : none;
-        border-left : none;
-        border-right: none;
-        border-width: 2px;
-        border-color: red;
-    }
     .dropbtn:hover,
     .dropbtn:focus {
         background-color: white;
-        border-top : none;
-        border-left : none;
+        border-top: none;
+        border-left: none;
         border-right: none;
         border-width: 2px;
         border-color: red;
     }
 
     .dropdown {
-        position: relative;
-        display: inline-block;
-
+        margin-left: 20%;
+        margin-right: 15%;
     }
 
     .dropdown-content {
         display: none;
-        position: ;
         background-color: white;
-        width: 140%;
+        width: 97.5%;
         overflow: auto;
         z-index: 1;
         padding: 5px;
         border-style: solid;
         border-color: #92a8d1;
         border-width: 1px;
+        flex-grow: 1;
+        top: 100%;
+        /* Posiciona abaixo do elemento pai */
+        right: 0;
+        ;
+        /* Alinha com a borda esquerda do elemento pai */
     }
 
     .dropdown-content a {
@@ -74,22 +60,16 @@
         display: block;
     }
 
-
     .dropdown a:hover {
         background-color: #ddd;
     }
 
     .show {
-        display: block;
+        display: inline-block;
     }
-    .timer{
-        width: 80px;
-    }
-        
-    
 </style>
-<div class="input-group">
-    <input type="text" class="flatpickr" id="datepicker" style="height: 40px; padding-left: 10px;"placeholder="escolha a data:">
+<div class="input-group" >
+    <input type="text" class="flatpickr" id="datepicker" style="height: 40px; padding-left: 10px;" placeholder="escolha a data:">
 
     <script>
         flatpickr("#datepicker", {
@@ -134,25 +114,50 @@
     </script>
 
 
-    <select name="timer" id="timer" placeholder="horas" class="timer" required disabled>
+    <select name="timer" id="timer" placeholder="horas" class="timer" style="height: 40px; padding-left: 10px" required disabled>
         <option value=" "> </option>
     </select>
     <script>
         document.getElementById("timer").addEventListener("change", function() {
-  myFunction();
-});
-    </script> 
+            myFunction();
+        });
+    </script>
+
+    <button onclick="myFunction()" style=" height: 40px; padding-left: 10px; width:60%;max-width:68%;" class="dropbtn">OS SEUS DADOS</button>
 
 
+</div>
+</div>
+<script>
+    /* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+    function myFunction() {
+        document.getElementById("myDropdown").classList.toggle("show");
+    }
 
+
+    /* Close the dropdown if the user clicks outside of it
+    window.onclick = function(event) {
+        if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }*/
+</script>
 <div class="dropdown" id="dropdown">
 
-    <button onclick="myFunction()" class="dropbtn">OS SEUS DADOS</button>
+
     <div id="myDropdown" class="dropdown-content">
         <br>
         <input type="text" id="nome" name="nome" placeholder="NOME*" required disabled>
 
-        <input type="email" id="email" name="email"placeholder="email*" required disabled>
+        <input type="email" id="email" name="email" placeholder="email*" required disabled>
 
         <input type="tel" id="tel" name="tel" pattern="[0-9]{9}" placeholder="Tel*" required disabled>
 
@@ -160,7 +165,7 @@
 
         <br><br>
 
-        <select name="category" id="category" placeholder="Categoria*"required disabled>
+        <select name="category" id="category" placeholder="Categoria*" required disabled>
             <option value="1">Ligeiro</option>
             <option value="2">Pesado</option>
             <option value="3">Trator</option>
@@ -172,8 +177,8 @@
             <option value="9">Trator > 3500KG</option>
         </select>
 
-        
-        <select name="motive" id="motive" placeholder="Motivo*"required disabled>
+
+        <select name="motive" id="motive" placeholder="Motivo*" required disabled>
             <option value="1">Inspeção Periodica</option>
             <option value="2">Reinspecção</option>
             <option value="3">Insp. p/atrib. nova matricula</option>
@@ -207,7 +212,7 @@
         <button type="submit" onclick="insertApp()" id="submit" style="margin:10px">Enviar</button>
 
         </form>
- 
+
         <script>
             var form = document.getElementById("myDropdown");
             var inputs = form.querySelectorAll("input, textarea, select");
@@ -216,7 +221,7 @@
                     for (var i = 0; i < inputs.length; i++) {
                         inputs[i].disabled = false;
                     }
-                   /* document.getElementById("dropdown").submit.disabled = false;*/
+                    /* document.getElementById("dropdown").submit.disabled = false;*/
                 } else {
                     for (var i = 0; i < inputs.length; i++) {
                         inputs[i].disabled = true;
@@ -265,32 +270,6 @@
         </script>
 
     </div>
-</div>
-</div>
-<script>
-    /* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-    function myFunction() {
-        document.getElementById("myDropdown").classList.toggle("show");
-    }
-    
+    </body>
 
-    /* Close the dropdown if the user clicks outside of it
-    window.onclick = function(event) {
-        if (!event.target.matches('.dropbtn')) {
-            var dropdowns = document.getElementsByClassName("dropdown-content");
-            var i;
-            for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                    openDropdown.classList.remove('show');
-                }
-            }
-        }
-    }*/
-
-</script>
-
-</body>
-
-</html>
+    </html>
